44c0c857b14aff93438ffd23dd447fe9234c5f3f
drewvu <andrewvu.cs@gmail.com>
2025-05-21 09:46:05 -0700

    freed up pizza component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+const Pizza = (props) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
+    ]);
+};
+
+
+
 const App = () => {
     return React.createElement(
         "div",
         {},
-        React.createElement("h1",{}, "Drew's Pizza Shop")
-    )
-}
+        [
+        React.createElement("h1",{}, "Drew's Pizza Shop"),
+        React.createElement(Pizza, {
+            name: "The Pepperoni Pizza",
+            description: " delecious Pepperoni",
+        }),
+        React.createElement(Pizza, {
+            name: "Americano Pizza",
+            description: "French fries and hot dogs"
+        }),
+        React.createElement(Pizza, {
+            name: "The Hawaiian",
+            description: "pineapple and ham"
+        }),
+        React.createElement(Pizza, {
+            name: "Chicken Pizza?",
+            description: " chicken nuggies on your pizza"
+        }),
+        React.createElement(Pizza, {
+            name: "baked potato",
+            description: "lots of potatoes there"
+        }),
+        ]
+    );
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
